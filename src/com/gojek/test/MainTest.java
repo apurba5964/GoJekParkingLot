bbdda4e5e37bf3b9c2d8b2d19c0c67710192b35a
apurba <apurba5964@gmail.com>
2017-09-07 08:52:15 +0530

    Unit Test case check in

---
+++ src/com/gojek/test/MainTest.java
@@ -0,0 +1,69 @@
+package com.gojek.test;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import com.gojek.main.ParkingApp;
+import com.gojek.parkinglot.ParkingLot;
+
+
+public class MainTest {
+	
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	
+//	 @Mock 
+//	 ParkingLot parkingLot;
+//     
+//     @InjectMocks 
+//     private ParkingApp parkingApp;
+	
+	
+	@Before
+	public void setStreams() {
+		System.setOut(new PrintStream(outContent));
+	}
+
+	@After
+	public void cleanUpStreams() {
+	    System.setOut(null);
+	}
+	
+//	@Test
+//	public void exitCommandTest() {
+//		System.setIn(inExitContent);
+//		Main.main(new String[0]);
+//		assertEquals("bye", outContent.toString());
+//	}
+	
+	@Test
+	public void createParkingLotTest() {
+		int slots = 10;
+		ParkingLot lot = new ParkingLot(slots);
+		assertEquals(slots, lot.getSlots().size());
+	}
+	
+//	@Test
+//	public void createCommandTest() {
+//		int numSlots = 6;
+//		String[] commandSplit = ("create_parking_lot " + numSlots + "").split(" ");
+//		//when(parkingLot).thenReturn(new ParkingLot(numSlots));
+//		//when(parkingApp).thenReturn(new ParkingApp());
+//		parkingApp.runCommand(commandSplit[0], commandSplit);
+//		assertEquals("Created a parking lot with " + numSlots +" slots", outContent.toString());
+//		outContent.reset();
+//	}
+	
+	@Test
+	public void unKnownCommandTest() {
+		ParkingApp app = new ParkingApp();
+		app.runCommand("sjkdgvkjsd", null);
+		assertTrue(outContent.toString().contains("Please try any of these commands: "));
+		outContent.reset();
+	}
+}
+	
